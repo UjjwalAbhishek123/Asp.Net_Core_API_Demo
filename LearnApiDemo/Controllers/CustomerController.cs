5db6bbc99bfbd816ecd168c33f7c52604609becf
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2024-10-22 19:50:38 +0530

    12th commit Export Excel Sheet functionality demo

--- LearnApiDemo/Controllers/CustomerController.cs
+++ LearnApiDemo/Controllers/CustomerController.cs
@@ -1,10 +1,12 @@
-﻿using LearnApiDemo.DTOs;
+﻿using ClosedXML.Excel;
+using LearnApiDemo.DTOs;
 using LearnApiDemo.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
+using System.Data;
 
 namespace LearnApiDemo.Controllers
 {
@@ -17,10 +19,13 @@ namespace LearnApiDemo.Controllers
     {
         //injecting Service
         private readonly ICustomerService _customerService;
-        
-        public CustomerController(ICustomerService customerService)
+
+        private readonly IWebHostEnvironment _environment;
+
+        public CustomerController(ICustomerService customerService, IWebHostEnvironment environment)
         {
             _customerService = customerService;
+            _environment = environment;
         }
 
         [AllowAnonymous]
@@ -74,6 +79,69 @@ namespace LearnApiDemo.Controllers
             var data = await _customerService.RemoveCustomer(code);
 
             return Ok(data);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("ExportExcel")]
+        public async Task<IActionResult> ExportExcel()
+        {
+            try
+            {
+                string filePath = GetFilepath();
+
+                string excelPath = filePath + "\\customerinfo.xlsx";
+
+                //initiate DataTable
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("Code", typeof(string));
+                dt.Columns.Add("Name", typeof(string));
+                dt.Columns.Add("Email", typeof(string));
+                dt.Columns.Add("Phone", typeof(string));
+                dt.Columns.Add("CreditLimit", typeof(int));
+
+                var data = await _customerService.GetAll();
+
+                if (data != null && data.Count > 0)
+                {
+                    data.ForEach(item =>
+                    {
+                        dt.Rows.Add(item.Code, item.Name, item.Email, item.Phone, item.CreditLimit);
+                    });
+                }
+
+                //initiate Excel Workbook
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    //add worksheet
+                    wb.AddWorksheet(dt, "Customer Info");
+
+                    //converting Excel workbook into memory stream
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+
+                        if (System.IO.File.Exists(excelPath))
+                        {
+                            System.IO.File.Delete(excelPath);
+                        }
+                        wb.SaveAs(excelPath);
+
+                        return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Customer.xlsx");
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        [NonAction]
+        private string GetFilepath()
+        {
+            return _environment.WebRootPath + "\\Export";
         }
     }
 }
